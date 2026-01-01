cc1f4c969a215534ddabc1bad6c65ad6e397a930
Nobu <metamototomo@gmail.com>
2026-01-01 18:39:04 +1100

    fixed animiation of humberger menu

--- static/js/custom.js
+++ static/js/custom.js
@@ -1,28 +1,9 @@
-// Fix cloned navbar sidebar menu
+// Fix cloned navbar sidebar menu - keep original theme animation
 $(document).ready(function(){
-    let sidebarOpen = false;
-    
-    // Handle both original and cloned menu icon triggers
-    $('.menu-icon-trigger, #cloned-menu-icon-trigger').on("click", function(e){
+    // Handle cloned menu icon trigger (same as original theme logic)
+    $('#cloned-menu-icon-trigger').on("click", function(e){
         e.preventDefault();
-        console.log('Hamburger clicked');
-        
-        $('.menu-icon-wrapper').toggleClass('open');
         $('#cloned-menu-icon-wrapper').toggleClass('open');
-        
-        // Direct class manipulation instead of toggleClass
-        if (sidebarOpen) {
-            $('.sidebar').removeClass('is-active');
-            sidebarOpen = false;
-        } else {
-            $('.sidebar').addClass('is-active');
-            sidebarOpen = true;
-        }
-        
-        console.log('Sidebar toggled, now open:', sidebarOpen);
-        console.log('Sidebar has is-active class:', $('.sidebar').hasClass('is-active'));
+        $('.sidebar').toggleClass('is-active');
     });
-    
-    // Debug: Check if sidebar exists
-    console.log('Sidebar elements found:', $('.sidebar').length);
 });
